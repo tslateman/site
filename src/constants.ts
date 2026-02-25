bf132e4d38a252e74c7b8034f6c886127653f02b
Tommy Slater <t.r.slater@gmail.com>
2026-02-25 07:21:19 -0800

    fix: Replace AstroPaper defaults with personal content
    Update social links to actual GitHub and LinkedIn profiles.
    Remove redundant hero title that duplicated the nav header.

--- src/constants.ts
+++ src/constants.ts
@@ -19,27 +19,15 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/tslateman",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
-    name: "X",
-    href: "https://x.com/username",
-    linkTitle: `${SITE.title} on X`,
-    icon: IconBrandX,
-  },
-  {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/tslateman/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
-  },
-  {
-    name: "Mail",
-    href: "mailto:yourmail@gmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
-    icon: IconMail,
   },
 ] as const;
 
